f4d36e3dc0467df8c1d6a302dde90894b2282265
moT01 <20648924+moT01@users.noreply.github.com>
2021-04-08 17:13:47 -0500

    930.1

--- .freeCodeCamp/test/920.test.js
+++ .freeCodeCamp/test/920.test.js
@@ -1,4 +1,4 @@
-const assert = require('assert');
+/*const assert = require('assert');
 const { getFileContents } = require('./utils');
 
 describe('Your script', () => {
@@ -7,4 +7,4 @@ describe('Your script', () => {
 
     assert(/INSERT_GPA_RESULT[\s\S]*if[ \t]+\[\[[ \t]+\$INSERT_GPA_RESULT[ \t]+==[ \t]+('|")INSERT 0 1\1[ \t]\]\]([ \t]*;\s*|\s*\n+\s*)then\s+echo[ \t]+('|"|)Successfully inserted \$GPA into gpas\3([ \t]*;\s*|\s*\n\s*)fi[\s\S]*fi/g.test(scriptFile));
   });
-});
+});*/
