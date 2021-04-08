0ba8d6d0b87c36c5335fdadc540eb95dd485c56a
moT01 <20648924+moT01@users.noreply.github.com>
2021-04-08 17:14:24 -0500

    1010.1

--- .freeCodeCamp/test/1000.test.js
+++ .freeCodeCamp/test/1000.test.js
@@ -1,4 +1,4 @@
-const assert = require('assert');
+/*const assert = require('assert');
 const { getFileContents } = require('./utils');
 
 describe('Your script', () => {
@@ -7,4 +7,4 @@ describe('Your script', () => {
 
     assert(/INSERT_MAJOR_RESULT[\s\S]*if[ \t]+\[\[[ \t]+\$INSERT_MAJOR_RESULT[ \t]+==[ \t]+('|")INSERT 0 1\1[ \t]\]\]([ \t]*;\s*|\s*\n+\s*)then\s+echo[ \t]+('|"|)Successfully inserted \$MAJOR into majors\3([ \t]*;\s*|\s*\n+\s*)fi[\s\S]*fi[\s\S]*fi/g.test(scriptFile));
   });
-});
+});*/
