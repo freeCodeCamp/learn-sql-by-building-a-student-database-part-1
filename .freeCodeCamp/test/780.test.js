04299954ada5b37d4821a03e04f5ae575dd9799c
moT01 <20648924+moT01@users.noreply.github.com>
2021-04-08 17:12:08 -0500

    790.1

--- .freeCodeCamp/test/780.test.js
+++ .freeCodeCamp/test/780.test.js
@@ -1,4 +1,4 @@
-const assert = require('assert');
+/*const assert = require('assert');
 const { getLastCommand, getCwd } = require('./utils');
 const { Client } = require('pg');
 
@@ -26,4 +26,4 @@ describe('You', () => {
       await client.end();
     }
   });
-});
+});*/
