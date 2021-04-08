b2ff4d239b4cc67df347a0b867207098664d4052
moT01 <20648924+moT01@users.noreply.github.com>
2021-04-08 17:10:30 -0500

    580.1

---
+++ .freeCodeCamp/test/580.test.js
@@ -0,0 +1,10 @@
+const assert = require('assert');
+const { getFileContents } = require('./utils');
+
+describe('Your script', () => {
+  it('should have the suggested command added correctly', async () => {
+    const scriptFile = await getFileContents('../insert_data.sh');
+
+    assert(/^[ \t]*INSERT_STUDENT_RESULT[\s\S]*[ \t]*echo[ \t]+\$INSERT_STUDENT_RESULT[ \t]*;?[ \t]*$/gm.test(scriptFile));
+  });
+});
