3d4cb274a7d0181701e0a4bf1e266f238aac05b4
moT01 <20648924+moT01@users.noreply.github.com>
2021-04-08 17:14:10 -0500

    980.1

---
+++ .freeCodeCamp/test/980.test.js
@@ -0,0 +1,10 @@
+const assert = require('assert');
+const { getFileContents } = require('./utils');
+
+describe('Your script', () => {
+  it('should have the suggested command added correctly', async () => {
+    const scriptFile = await getFileContents('../insert_data.sh');
+
+    assert(/MAJOR_ID[\s\S]*if[ \t]+\[\[[ \t]+-z[ \t]+\$MAJOR_ID[ \t]+\]\]([ \t]*;\s*|\s*\n+\s*)then\s+fi[\s\S]*fi/gm.test(scriptFile));
+  });
+});
