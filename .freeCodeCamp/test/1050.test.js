374455400696cf25e960c2779d2cad6d8e32ac62
moT01 <20648924+moT01@users.noreply.github.com>
2021-04-08 17:14:44 -0500

    1050.1

---
+++ .freeCodeCamp/test/1050.test.js
@@ -0,0 +1,10 @@
+const assert = require('assert');
+const { getFileContents } = require('./utils');
+
+describe('Your script', () => {
+  it('should have the suggested variable added correctly', async () => {
+    const scriptFile = await getFileContents('../insert_data.sh');
+
+    assert(/INSERT_MAJOR_RESULT[\s\S]*fi[\s\S]*MAJOR_ID=\$\([ \t]*\$PSQL[ \t]*"SELECT[ \t]+major_id[ \t]+FROM[ \t]+majors[ \t]+WHERE[ \t]+major[ \t]*=[ \t]*'\$MAJOR'[ \t]*;?[ \t]*"[ \t]*\)[\s\S]*fi[\s\S]*fi/gm.test(scriptFile));
+  });
+});
