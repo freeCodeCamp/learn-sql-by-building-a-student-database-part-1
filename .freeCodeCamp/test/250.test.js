beb82194f1962f518d8c81abd5974e85ab252bc1
moT01 <20648924+moT01@users.noreply.github.com>
2021-04-08 17:07:35 -0500

    250.1

---
+++ .freeCodeCamp/test/250.test.js
@@ -0,0 +1,26 @@
+const assert = require('assert');
+const { Client } = require('pg');
+
+const database = 'students';
+const connectionString = `postgresql://postgres@localhost:5432/${database}`;
+
+const client = new Client({
+  connectionString: connectionString,
+});
+
+describe('The "majors_courses" table', () => {
+  it('should the correct "course_id" column', async () => {
+    const query = `SELECT column_name FROM INFORMATION_SCHEMA.COLUMNS WHERE table_schema = 'public' AND table_name='majors_courses' AND column_name='course_id' AND data_type='integer'`;
+
+    try {
+      await client.connect();
+      const res = await client.query(query);
+
+      assert(res.rowCount > 0);
+    } catch (err) {
+      assert(false);
+    } finally {
+      await client.end();
+    }
+  });
+});
