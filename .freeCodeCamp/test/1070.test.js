592e028148bd74d8546072e70832c6811a006451
moT01 <20648924+moT01@users.noreply.github.com>
2021-04-08 17:14:53 -0500

    1070.1

---
+++ .freeCodeCamp/test/1070.test.js
@@ -0,0 +1,10 @@
+const assert = require('assert');
+const { getFileContents } = require('./utils');
+
+describe('Your script', () => {
+  it('should have the suggested command added correctly', async () => {
+    const scriptFile = await getFileContents('../insert_data.sh');
+
+    assert(/INSERT_STUDENTS_MAJORS_RESULT[\s\S]*if[ \t]+\[\[[ \t]+\$INSERT_STUDENTS_MAJORS_RESULT[ \t]+==[ \t]+('|")INSERT 0 1\1[ \t]\]\]([ \t]*;\s*|\s*\n+\s*)then\s+echo[ \t]+('|"|)Successfully inserted row into students_majors\3([ \t]*;?\s*|\s*\n+\s*)fi[\s\S]*fi/g.test(scriptFile));
+  });
+});
