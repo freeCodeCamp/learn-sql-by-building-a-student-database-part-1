58850d93f19af0f47060d8dde0164de6b4b55051
moT01 <20648924+moT01@users.noreply.github.com>
2021-04-08 17:10:14 -0500

    550.1

--- .freeCodeCamp/test/540.test.js
+++ .freeCodeCamp/test/540.test.js
@@ -1,4 +1,4 @@
-const assert = require('assert');
+/*const assert = require('assert');
 const { getFileContents } = require('./utils');
 
 describe('Your script', () => {
@@ -7,4 +7,4 @@ describe('Your script', () => {
 
     assert(!/(echo|\$FIRST|\$LAST|\$MAJOR|\$GPA)/gm.test(scriptFile));
   });
-});
+});*/
---
+++ .freeCodeCamp/test/550.test.js
@@ -0,0 +1,10 @@
+const assert = require('assert');
+const { getFileContents } = require('./utils');
+
+describe('Your script', () => {
+  it('should all the suggested comments added correctly', async () => {
+    const scriptFile = await getFileContents('../insert_data.sh');
+
+    assert(/[\s\S]+#[ \t]*insert student[\s\S]+#[ \t]*get student_id[\s\S]+#[ \t]*insert gpa[\s\S]+#[ \t]*get major_id[\s\S]+#[ \t]*insert major if not found[\s\S]+#[ \t]*get new major_id[\s\S]+#[ \t]*insert into students_majors[\s\S]+/g.test(scriptFile));
+  });
+});
