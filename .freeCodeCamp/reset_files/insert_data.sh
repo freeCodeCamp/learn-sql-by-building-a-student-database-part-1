9a40ef035ebf6cc1bc9014262dbd12bd3be1d931
moT01 <20648924+moT01@users.noreply.github.com>
2021-04-08 17:13:27 -0500

    890.1

--- .freeCodeCamp/reset_files/insert_data.sh
+++ .freeCodeCamp/reset_files/insert_data.sh
@@ -3,6 +3,7 @@
 # Script to insert data from courses.csv and students.csv into students database
 
 PSQL="psql -X --username=freecodecamp --dbname=students --no-align --tuples-only -c"
+echo $($PSQL "TRUNCATE students, majors, courses, majors_courses")
 
 cat courses_test.csv | while IFS="," read MAJOR COURSE
 do
