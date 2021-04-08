de71b7f5bfd54e20b71485adcb03e6d980d65f8a
moT01 <20648924+moT01@users.noreply.github.com>
2021-04-08 17:11:03 -0500

    650.1

--- .freeCodeCamp/test/650.test.js
+++ .freeCodeCamp/test/650.test.js
@@ -1,11 +1,11 @@
-/*const assert = require('assert');
+const assert = require('assert');
 const { getLastLog } = require('./utils');
 
 describe('You', () => {
   it('should enter the suggested command', async () => {
     const lastLog = await getLastLog(true);
-    const re = /^statement:truncatemajors;freecodecampERROR:/i;
+    const re = /^statement:truncate(majors,students|students,majors);freecodecampERROR:/i;
 
     assert(re.test(lastLog));
   });
-});*/
+});
