5fef05d1dd14f584a4f232930e489ee7eb2558e1
moT01 <20648924+moT01@users.noreply.github.com>
2021-04-08 17:10:34 -0500

    590.1

--- .freeCodeCamp/test/580.test.js
+++ .freeCodeCamp/test/580.test.js
@@ -1,4 +1,4 @@
-const assert = require('assert');
+/*const assert = require('assert');
 const { getFileContents } = require('./utils');
 
 describe('Your script', () => {
@@ -7,4 +7,4 @@ describe('Your script', () => {
 
     assert(/^[ \t]*INSERT_MAJOR_RESULT[\s\S]*[ \t]*echo[ \t]+\$INSERT_MAJOR_RESULT[ \t]*;?[ \t]*$/gm.test(scriptFile));
   });
-});
+});*/
