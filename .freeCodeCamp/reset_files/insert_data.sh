ffb384344fc16f42576855e2cd181225904bb586
moT01 <20648924+moT01@users.noreply.github.com>
2021-04-08 17:12:21 -0500

    820.1

--- .freeCodeCamp/reset_files/insert_data.sh
+++ .freeCodeCamp/reset_files/insert_data.sh
@@ -16,6 +16,10 @@ do
     then
       # insert major
       INSERT_MAJOR_RESULT=$($PSQL "INSERT INTO majors(major) VALUES('$MAJOR')")
+      if [[ $INSERT_MAJOR_RESULT == "INSERT 0 1" ]]
+      then
+        echo Inserted into majors, $MAJOR
+      fi
 
       # get new major_id
 
