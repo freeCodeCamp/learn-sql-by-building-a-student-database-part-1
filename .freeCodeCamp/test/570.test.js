e024e4e43c1ac058a02f3f69a3cfb7b7028c6831
moT01 <20648924+moT01@users.noreply.github.com>
2021-04-08 17:10:25 -0500

    570.1

---
+++ .freeCodeCamp/test/570.test.js
@@ -0,0 +1,12 @@
+const assert = require('assert');
+const { getFileContents } = require('./utils');
+
+describe('Your script', () => {
+  it('should have the suggested variable added correctly', async () => {
+    const scriptFile = await getFileContents('../insert_data.sh');
+    const test1 = /do[\s\S]*[ \t]*INSERT_STUDENT_RESULT=\$\([ \t]*\$PSQL[ \t]*"INSERT[ \t]+INTO[ \t]+students[ \t]*\([ \t]*first_name[ \t]*,[ \t]*last_name[ \t]*\)[ \t]*VALUES[ \t]*\([ \t]*'\$FIRST'[ \t]*,[ \t]*'\$LAST'[ \t]*\)[ \t]*;?[ \t]*"[ \t]*\)[ \t]*;?[ \t]*[\s\S]*done/gi.test(scriptFile)
+    const test2 = /INSERT_STUDENT_RESULT=\$\([ \t]*\$PSQL[\s\S]*'\$FIRST'[ \t]*,[ \t]*'\$LAST'/g.test(scriptFile);
+
+    assert(test1 && test2);
+  });
+});
