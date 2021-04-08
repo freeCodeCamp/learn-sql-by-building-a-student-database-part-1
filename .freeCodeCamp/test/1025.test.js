fb27efb0677827168316cc6fb88d75ba8aad073c
moT01 <20648924+moT01@users.noreply.github.com>
2021-04-08 17:14:33 -0500

    1030.1

--- .freeCodeCamp/test/1025.test.js
+++ .freeCodeCamp/test/1025.test.js
@@ -1,4 +1,4 @@
-const assert = require('assert');
+/*const assert = require('assert');
 const { getFileContents } = require('./utils');
 
 describe('Your script', () => {
@@ -7,4 +7,4 @@ describe('Your script', () => {
 
     assert(/first_name[\s\S]*then\s*#[ \t]*get major_id\s*\n+\s*#[ \t]*if not found\s*\n+\s*#[ \t]*set to null\s*\n+\s*#[ \t]*insert student\s*\n+\s*fi\s*\n+\s*done/g.test(scriptFile));
   });
-});
+});*/
