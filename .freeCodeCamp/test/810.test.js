e87fb09b34c1bd318964f0f9d86b7a76ded9ab83
moT01 <20648924+moT01@users.noreply.github.com>
2021-04-08 17:12:21 -0500

    820.1

--- .freeCodeCamp/test/810.test.js
+++ .freeCodeCamp/test/810.test.js
@@ -1,4 +1,4 @@
-const assert = require('assert');
+/*const assert = require('assert');
 const { getFileContents } = require('./utils');
 
 describe('Your script', () => {
@@ -7,4 +7,4 @@ describe('Your script', () => {
 
     assert(/then[\s\S]*INSERT_STUDENT_RESULT[\s\S]*if[ \t]+\[\[[ \t]+\$INSERT_STUDENT_RESULT[ \t]+==[ \t]+('|")INSERT 0 1\1[ \t]\]\]([ \t]*;\s*|\s*\n\s*)then\s+echo[ \t]+('|"|)Successfully inserted \$FIRST \$LAST into students\3([ \t]*;\s*|\s*\n\s*)fi[\s\S]*fi/gm.test(scriptFile));
   });
-});
+});*/
