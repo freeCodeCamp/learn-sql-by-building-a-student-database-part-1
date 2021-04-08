b10b10c74ada7ce8f0073fd6df4d6474bb6a4e32
moT01 <20648924+moT01@users.noreply.github.com>
2021-04-08 17:10:39 -0500

    600.1

--- .freeCodeCamp/test/590.test.js
+++ .freeCodeCamp/test/590.test.js
@@ -1,4 +1,4 @@
-const assert = require('assert');
+/*const assert = require('assert');
 const { getLastCommand, getDirectoryContents, getCwd } = require('./utils');
 
 describe('You', () => {
@@ -9,4 +9,4 @@ describe('You', () => {
 
     assert(directoryContents.includes('students_test.csv') && lastCommand[0] === 'cp' && /students\.csv/g.test(lastCommand[1]) && /students_test\.csv/g.test(lastCommand[2]) && cwd === '/home/strove/project');
   });
-});
+});*/
---
+++ .freeCodeCamp/test/600.test.js
@@ -0,0 +1,11 @@
+const assert = require('assert');
+const { getFileContents } = require('./utils');
+
+describe('Your "students_test.csv" file', () => {
+  it('should have the correct rows', async () => {
+    const fileContents = await getFileContents('../students_test.csv');
+    const re = /^first_name,last_name,major,gpa\nRhea,Kellems,Database Administration,2\.5\nKimberly,Whitley,Web Development,3\.8\nJimmy,Felipe,Database Administration,3\.7\n$/g
+
+    assert(re.test(fileContents));
+  });
+});
