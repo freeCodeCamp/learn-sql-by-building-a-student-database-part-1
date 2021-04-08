793262af13b23ec580785487edf962c71c867df3
moT01 <20648924+moT01@users.noreply.github.com>
2021-04-08 17:16:09 -0500

    1230.1

--- .freeCodeCamp/reset.sh
+++ .freeCodeCamp/reset.sh
@@ -2,3 +2,4 @@ cp ./.freeCodeCamp/reset_files/students.csv .
 cp ./.freeCodeCamp/reset_files/courses.csv .
 psql -U postgres < ./.freeCodeCamp/reset_files/db.sql
 cp ./.freeCodeCamp/reset_files/insert_data.sh .
+cp ./.freeCodeCamp/reset_files/student_info.sh .
