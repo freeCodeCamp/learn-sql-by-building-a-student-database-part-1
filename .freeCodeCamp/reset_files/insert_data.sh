83f886fdc1cb9f4925e1cd39e2a8c74192f839a8
moT01 <20648924+moT01@users.noreply.github.com>
2021-04-08 17:14:53 -0500

    1070.1

--- .freeCodeCamp/reset_files/insert_data.sh
+++ .freeCodeCamp/reset_files/insert_data.sh
@@ -67,6 +67,6 @@ do
     fi
 
     # insert student
-
+    INSERT_STUDENT_RESULT=$($PSQL "INSERT INTO students(first_name, last_name, major_id, gpa) VALUES('$FIRST', '$LAST', $MAJOR_ID, $GPA)")
   fi
 done
