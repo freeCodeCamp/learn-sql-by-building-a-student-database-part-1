6167811fa1066e26ed79c701be6e497c6876309f
moT01 <20648924+moT01@users.noreply.github.com>
2021-04-08 17:10:14 -0500

    550.1

--- .freeCodeCamp/test/540.test.js
+++ .freeCodeCamp/test/540.test.js
@@ -1,4 +1,4 @@
-const assert = require('assert');
+/*const assert = require('assert');
 const { getFileContents } = require('./utils');
 
 describe('Your script', () => {
@@ -7,4 +7,4 @@ describe('Your script', () => {
 
     assert(!/(echo|\$MAJOR|\$COURSE)/gm.test(scriptFile));
   });
-});
+});*/
