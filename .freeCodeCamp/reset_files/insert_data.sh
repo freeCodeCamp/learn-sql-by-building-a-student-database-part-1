1e1aef8879a0a68a131569bec069a8db19b81bb7
moT01 <20648924+moT01@users.noreply.github.com>
2021-04-08 17:14:58 -0500

    1080.1

--- .freeCodeCamp/reset_files/insert_data.sh
+++ .freeCodeCamp/reset_files/insert_data.sh
@@ -68,5 +68,9 @@ do
 
     # insert student
     INSERT_STUDENT_RESULT=$($PSQL "INSERT INTO students(first_name, last_name, major_id, gpa) VALUES('$FIRST', '$LAST', $MAJOR_ID, $GPA)")
+    if [[ $INSERT_STUDENT_RESULT == "INSERT 0 1" ]]
+    then
+      echo Inserted into students, $FIRST $LAST
+    fi
   fi
 done
