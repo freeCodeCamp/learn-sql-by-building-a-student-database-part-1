94ed3101a542732b8260d1bf561236ae346dda02
moT01 <20648924+moT01@users.noreply.github.com>
2021-04-08 17:07:53 -0500

    260.1

---
+++ .freeCodeCamp/test/260.test.js
@@ -0,0 +1,30 @@
+const assert = require('assert');
+const { Client } = require('pg');
+
+const database = 'students';
+const connectionString = `postgresql://postgres@localhost:5432/${database}`;
+
+const client = new Client({
+  connectionString: connectionString,
+});
+
+describe('The "majors_courses" table', () => {
+  it('should have the correct "course_id" foreign key', async () => {
+    const query = `SELECT tc.table_name, kcu.column_name, ccu.table_name AS foreign_table_name, ccu.column_name AS foreign_column_name FROM information_schema.table_constraints tc JOIN information_schema.key_column_usage kcu ON tc.constraint_name = kcu.constraint_name JOIN information_schema.constraint_column_usage ccu ON ccu.constraint_name = tc.constraint_name WHERE constraint_type = 'FOREIGN KEY'`;
+
+    try {
+      await client.connect();
+      const res = await client.query(query);
+
+      const foreignKey = res.rows.filter(row => {
+        return row.table_name === 'majors_courses' && row.column_name === 'course_id' && row.foreign_table_name === 'courses' && row.foreign_column_name === 'course_id';
+      });
+
+      assert(foreignKey.length > 0);
+    } catch (err) {
+      assert(false);
+    } finally {
+      await client.end();
+    }
+  });
+});
