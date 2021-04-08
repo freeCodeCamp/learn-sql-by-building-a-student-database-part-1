091203c08b15cc767b357fc240c097c031f3d374
moT01 <20648924+moT01@users.noreply.github.com>
2021-04-08 17:10:20 -0500

    560.1

--- .freeCodeCamp/test/550.test.js
+++ .freeCodeCamp/test/550.test.js
@@ -1,4 +1,4 @@
-const assert = require('assert');
+/*const assert = require('assert');
 const { getFileContents } = require('./utils');
 
 describe('Your script', () => {
@@ -7,4 +7,4 @@ describe('Your script', () => {
 
     assert(/do(\s*\n+\s*)#[ \t]*get major_id(\s*\n+\s*)#[ \t]*if not found(\s*\n+\s*)#[ \t]*insert major(\s*\n+\s*)#[ \t]*get new major_id(\s*\n+\s*)#[ \t]*get course_id(\s*\n+\s*)#[ \t]*if not found(\s*\n+\s*)#[ \t]*insert course(\s*\n+\s*)#[ \t]*get new course_id(\s*\n+\s*)#[ \t]*insert into majors_courses(\s*\n+\s*)done/g.test(scriptFile));
   });
-});
+});*/
