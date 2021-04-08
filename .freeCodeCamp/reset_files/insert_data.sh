25009df29e0c4859f06a92f50ca9eee26385ae8b
moT01 <20648924+moT01@users.noreply.github.com>
2021-04-08 17:10:04 -0500

    530.1

--- .freeCodeCamp/reset_files/insert_data.sh
+++ .freeCodeCamp/reset_files/insert_data.sh
@@ -2,7 +2,7 @@
 
 # Script to insert data from courses.csv and students.csv into students database
 
-cat courses.csv | while read MAJOR COURSE
+cat courses.csv | while IFS="," read MAJOR COURSE
 do
   echo $MAJOR
 done
