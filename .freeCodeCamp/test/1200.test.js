1f96b654c489cd5a1f72140215b0e564602cf1f7
moT01 <20648924+moT01@users.noreply.github.com>
2021-04-08 17:16:00 -0500

    1210.1

--- .freeCodeCamp/test/1200.test.js
+++ .freeCodeCamp/test/1200.test.js
@@ -1,4 +1,4 @@
-const assert = require('assert');
+/*const assert = require('assert');
 const { getLastCommand, getDirectoryContents, getCwd } = require('./utils');
 
 describe('You', () => {
@@ -9,4 +9,4 @@ describe('You', () => {
 
     assert(!directoryContents.includes('students_test.csv') && lastCommand[0] === 'rm' && /students_test\.csv/g.test(lastCommand[1]) && cwd === '/home/strove/project');
   });
-});
+});*/
