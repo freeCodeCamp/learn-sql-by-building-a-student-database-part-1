bff6ce3c1262ea6ed679364bb4442b92f3afa0a5
moT01 <20648924+moT01@users.noreply.github.com>
2021-04-16 14:21:08 -0500

    566.1

--- .freeCodeCamp/reset_files/insert_data.sh
+++ .freeCodeCamp/reset_files/insert_data.sh
@@ -8,6 +8,7 @@ cat courses.csv | while IFS="," read MAJOR COURSE
 do
   # get major_id
   MAJOR_ID=$($PSQL "SELECT major_id FROM majors WHERE major='$MAJOR'")
+  echo $MAJOR_ID
 
   # if not found
 
