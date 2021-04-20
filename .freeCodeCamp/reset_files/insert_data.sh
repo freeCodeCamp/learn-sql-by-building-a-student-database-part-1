f3f94458a023ba281f9a4ec045285e71b88b242b
moT01 <20648924+moT01@users.noreply.github.com>
2021-04-20 10:34:01 -0500

    1049.1

--- .freeCodeCamp/reset_files/insert_data.sh
+++ .freeCodeCamp/reset_files/insert_data.sh
@@ -57,17 +57,17 @@ do
   if [[ $FIRST != "first_name" ]]
   then
     # get major_id
-    MAJOR_ID=$($PSQL "SELECT major_id FROM majors WHERE major='$MAJOR'")
+    MAJOR_ID=$($PSQL "SELECT major_id FROM majors WHERE major='$MAJOR'") 
     echo $MAJOR_ID
-    
+
     # if not found
     if [[ -z $MAJOR_ID ]]
     then
       # set to null
+      MAJOR_ID=null
+    fi   
 
-    fi
-
-    # insert student
+   # insert student
 
   fi
 done
