1b20730a37d030e4fdde8830260af3f5bc1ad456
moT01 <20648924+moT01@users.noreply.github.com>
2021-05-04 14:59:06 -0500

    2390.1

--- .freeCodeCamp/test/2390.test.js
+++ .freeCodeCamp/test/2390.test.js
@@ -1,11 +1,11 @@
-/*const assert = require('assert');
+const assert = require('assert');
 const { getLastLog } = require('./utils');
 
 describe('You', () => {
   it('should enter the correct command in the psql prompt', async () => {
     const lastLog = await getLastLog(true);
-    const query = /statement:selects\.major_idfrom(studentsassfull(outer)?joinmajorsasm|majorsasmfull(outer)?joinstudentsass)on(s.major_id=m\.major_id|m\.major_id=s\.major_id);/i;
+    const query = /statement:select\*from(studentsfull(outer)?joinmajors|majorsfull(outer)?joinstudents)using\(major_id\);/i;
 
     assert(query.test(lastLog) && !/ERROR/.test(lastLog));
   });
-});*/
+});
