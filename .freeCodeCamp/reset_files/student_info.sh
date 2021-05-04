85457aac8cd850b8fcd3f1cfd09bdb0bd68ae11e
moT01 <20648924+moT01@users.noreply.github.com>
2021-05-04 14:58:04 -0500

    2320.1

--- .freeCodeCamp/reset_files/student_info.sh
+++ .freeCodeCamp/reset_files/student_info.sh
@@ -31,3 +31,4 @@ echo -e "\nMajor ID, total number of students in a column named 'number_of_stude
 echo "$($PSQL "SELECT major_id, COUNT(*) AS number_of_students, ROUND(AVG(gpa), 2) AS average_gpa FROM students GROUP BY major_id HAVING COUNT(*) > 1")"
 
 echo -e "\nList of majors, in alphabetical order, that either no student is taking or has a student whose first name contains a case insensitive 'ma':"
+echo "$($PSQL "SELECT major FROM students FULL JOIN majors ON students.major_id = majors.major_id WHERE major IS NOT NULL AND (student_id IS NULL OR first_name ILIKE '%ma%') ORDER BY major")"
