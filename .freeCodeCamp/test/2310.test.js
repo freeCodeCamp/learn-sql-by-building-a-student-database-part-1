231ab9dbf2215e400d45a26f25444f1ddd137e8e
moT01 <20648924+moT01@users.noreply.github.com>
2021-05-04 14:58:04 -0500

    2320.1

--- .freeCodeCamp/test/2310.test.js
+++ .freeCodeCamp/test/2310.test.js
@@ -1,4 +1,4 @@
-const assert = require('assert');
+/*const assert = require('assert');
 const { getCommandOutput } = require('./utils');
 
 describe('Your script', () => {
@@ -7,4 +7,4 @@ describe('Your script', () => {
 
     assert(/\n(Computer Programming\n|Database Administration\n|Network Engineering|\nWeb Development\n){4}$/g.test(output));
   });
-});
+});*/
