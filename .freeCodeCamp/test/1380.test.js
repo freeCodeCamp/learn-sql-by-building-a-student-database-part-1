6f0f5ff4370fdfdb174733f8b3f1829a482cefea
moT01 <20648924+moT01@users.noreply.github.com>
2021-05-04 14:42:41 -0500

    1390.1

--- .freeCodeCamp/test/1380.test.js
+++ .freeCodeCamp/test/1380.test.js
@@ -1,4 +1,4 @@
-const assert = require('assert');
+/*const assert = require('assert');
 const { getCommandOutput } = require('./utils');
 
 describe('Your script', () => {
@@ -7,4 +7,4 @@ describe('Your script', () => {
 
     assert(/\n\nAll course names whose first letter is before 'D' in the alphabet:\n{1}$/g.test(output));
   });
-});
+});*/
