6864226782a3378f602d5edf231978ced7c22327
moT01 <20648924+moT01@users.noreply.github.com>
2021-05-04 14:43:18 -0500

    1450.1

---
+++ .freeCodeCamp/test/1450.test.js
@@ -0,0 +1,10 @@
+const assert = require('assert');
+const { getCommandOutput } = require('./utils');
+
+describe('Your script', () => {
+  it('should print the results of the suggested query correctly', async () => {
+    const output = await getCommandOutput('./student_info.sh');
+
+    assert(/\nComputer Networks\nComputer Systems\nArtificial Intelligence\nCalculus\nAlgorithms\n{1}$/g.test(output));
+  });
+});
