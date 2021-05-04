8e1c39ad6ff91f15d71b77788c7441f5ccce8cc0
moT01 <20648924+moT01@users.noreply.github.com>
2021-05-04 14:43:36 -0500

    1480.1

--- .freeCodeCamp/test/1470.test.js
+++ .freeCodeCamp/test/1470.test.js
@@ -1,4 +1,4 @@
-const assert = require('assert');
+/*const assert = require('assert');
 const { getCommandOutput } = require('./utils');
 
 describe('Your script', () => {
@@ -7,4 +7,4 @@ describe('Your script', () => {
 
     assert(/\n\nFirst name, last name, and GPA of students whose last name begins with an 'R' or after and have a GPA greater than 3\.8 or less than 2\.0:\n{1}$/g.test(output));
   });
-});
+});*/
