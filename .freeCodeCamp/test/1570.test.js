809b51fa86dbd8756ee007ad8fea5c9ce18c1ed1
moT01 <20648924+moT01@users.noreply.github.com>
2021-05-04 14:44:28 -0500

    1570.1

---
+++ .freeCodeCamp/test/1570.test.js
@@ -0,0 +1,11 @@
+const assert = require('assert');
+const { getLastLog } = require('./utils');
+
+describe('You', () => {
+  it('should enter the correct command in the psql prompt', async () => {
+    const lastLog = await getLastLog(true);
+    const query = /statement:select(\*|course)fromcourses;/i;
+
+    assert(query.test(lastLog) && !/ERROR/.test(lastLog));
+  });
+});
