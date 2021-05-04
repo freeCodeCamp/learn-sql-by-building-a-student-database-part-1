20234ed7a28b27ff764805d3a8ef1d7866343111
moT01 <20648924+moT01@users.noreply.github.com>
2021-05-04 14:42:41 -0500

    1390.1

--- .freeCodeCamp/reset_files/student_info.sh
+++ .freeCodeCamp/reset_files/student_info.sh
@@ -8,3 +8,5 @@ PSQL="psql -X --username=freecodecamp --dbname=students --no-align --tuples-only
 
 echo -e "\nFirst name, last name, and GPA of students with a 4.0 GPA:"
 echo "$($PSQL "SELECT first_name, last_name, gpa FROM students WHERE gpa = 4.0")"
+
+echo -e "\nAll course names whose first letter is before 'D' in the alphabet:"
