1d933e42b43790e2695306032a6e1e013f171d5c
moT01 <20648924+moT01@users.noreply.github.com>
2021-05-04 14:41:48 -0500

    1290.1

--- .freeCodeCamp/reset_files/student_info.sh
+++ .freeCodeCamp/reset_files/student_info.sh
@@ -5,3 +5,5 @@
 echo -e "\n~~ My Computer Science Students ~~\n"
 
 PSQL="psql -X --username=freecodecamp --dbname=students --no-align --tuples-only -c"
+
+echo -e "\nFirst name, last name, and GPA of students with a 4.0 GPA:"
