d1d0f015c30995a53a2372f8f1b2b6fc03eb4afe
moT01 <20648924+moT01@users.noreply.github.com>
2021-05-04 14:52:19 -0500

    1960.1

---
+++ .freeCodeCamp/test/1960.test.js
@@ -0,0 +1,10 @@
+const assert = require('assert');
+const { getCommandOutput } = require('./utils');
+
+describe('Your script', () => {
+  it('should print the suggested line correctly', async () => {
+    const output = await getCommandOutput('./student_info.sh');
+
+    assert(/\n\nMajor ID, total number of students in a column named 'number_of_students', and average GPA rounded to two decimal places in a column name 'average_gpa', for each major ID in the students table having a student count greater than 1:\n{1}$/g.test(output));
+  });
+});
