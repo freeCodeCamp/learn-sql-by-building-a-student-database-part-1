1123272017de1f18b05bd1a11db87ba529fcbcf7
moT01 <20648924+moT01@users.noreply.github.com>
2021-05-04 14:41:34 -0500

    1280.1

--- .freeCodeCamp/reset_files/student_info.sh
+++ .freeCodeCamp/reset_files/student_info.sh
@@ -3,3 +3,5 @@
 # Info about my computer science students from students database
 
 echo -e "\n~~ My Computer Science Students ~~\n"
+
+PSQL="psql -X --username=freecodecamp --dbname=students --no-align --tuples-only -c"
