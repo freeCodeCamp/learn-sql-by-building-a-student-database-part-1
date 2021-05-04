1998d3cca47f3ab59c970f04e6050a2766ad11c7
moT01 <20648924+moT01@users.noreply.github.com>
2021-05-04 14:51:51 -0500

    1940.1

---
+++ .freeCodeCamp/test/1940.test.js
@@ -0,0 +1,10 @@
+const assert = require('assert');
+const { getCommandOutput } = require('./utils');
+
+describe('Your script', () => {
+  it('should print the results of the suggested query correctly', async () => {
+    const output = await getCommandOutput('./student_info.sh');
+
+    assert(/\n3\.09\n{1}$/g.test(output));
+  });
+});
