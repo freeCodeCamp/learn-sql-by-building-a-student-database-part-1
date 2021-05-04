fcdddbf9ae6a6bde888f94ba9102bc8f3e4ade6f
moT01 <20648924+moT01@users.noreply.github.com>
2021-05-04 14:44:18 -0500

    1550.1

--- .freeCodeCamp/test/1540.test.js
+++ .freeCodeCamp/test/1540.test.js
@@ -1,4 +1,4 @@
-const assert = require('assert');
+/*const assert = require('assert');
 const { getCommandOutput } = require('./utils');
 
 describe('Your script', () => {
@@ -7,4 +7,4 @@ describe('Your script', () => {
 
     assert(/\n(Efren\|Reilly\|3\.9\n|Mariana\|Russel\|1\.8\n|Mehdi\|Vandenberghe\|1\.9\n){3}$/g.test(output));
   });
-});
+});*/
