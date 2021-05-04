ed8f937b9b20706a5fc138e1daf724557b3f0f74
moT01 <20648924+moT01@users.noreply.github.com>
2021-05-04 14:53:43 -0500

    2100.1

--- .freeCodeCamp/reset_files/student_info.sh
+++ .freeCodeCamp/reset_files/student_info.sh
@@ -28,3 +28,4 @@ echo -e "\nAverage GPA of all students rounded to two decimal places:"
 echo "$($PSQL "SELECT ROUND(AVG(gpa), 2) FROM students")"
 
 echo -e "\nMajor ID, total number of students in a column named 'number_of_students', and average GPA rounded to two decimal places in a column name 'average_gpa', for each major ID in the students table having a student count greater than 1:"
+echo "$($PSQL "SELECT major_id, COUNT(*) AS number_of_students, ROUND(AVG(gpa), 2) AS average_gpa FROM students GROUP BY major_id HAVING COUNT(*) > 1")"
