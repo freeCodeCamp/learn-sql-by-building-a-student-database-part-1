2f58a5fc8e6d11005cac71cf83b4d9ae0f8ef147
moT01 <20648924+moT01@users.noreply.github.com>
2021-05-04 14:53:13 -0500

    2060.1

---
+++ .freeCodeCamp/test/2060.test.js
@@ -0,0 +1,11 @@
+const assert = require('assert');
+const { getLastLog } = require('./utils');
+
+describe('You', () => {
+  it('should enter the correct command in the psql prompt', async () => {
+    const lastLog = await getLastLog(true);
+    const query = /statement:select(major_id,min\(gpa\)asmin_gpa,max\(gpa\)|major_id,min\(gpa\)asmin_gpa,max\(gpa\)|min\(gpa\)asmin_gpa,major_id,max\(gpa\)|min\(gpa\)asmin_gpa,max\(gpa\),major_id|max\(gpa\),major_id,min\(gpa\)asmin_gpa|max\(gpa\),min\(gpa\)asmin_gpa,major_id)fromstudentsgroupbymajor_idhavingmax\(gpa\)=4(\.0)?;/i;
+
+    assert(query.test(lastLog) && !/ERROR/.test(lastLog));
+  });
+});
