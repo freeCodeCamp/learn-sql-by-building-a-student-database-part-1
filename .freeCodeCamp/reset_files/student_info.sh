8380a54f8548dcb95a391b4744d450721ef22c4e
moT01 <20648924+moT01@users.noreply.github.com>
2021-05-07 23:55:11 -0500

    1715.1

--- .freeCodeCamp/reset_files/student_info.sh
+++ .freeCodeCamp/reset_files/student_info.sh
@@ -17,3 +17,5 @@ echo "$($PSQL "SELECT first_name, last_name, gpa FROM students WHERE last_name >
 
 echo -e "\nLast name of students whose last name contains a case insensitive 'sa' or have an 'r' as the second to last letter:"
 echo "$($PSQL "SELECT last_name FROM students WHERE last_name ILIKE '%sa%' OR last_name ILIKE '%r_'")"
+
+echo -e "\nFirst name, last name, and GPA of students who have not selected a major and either their first name begins with 'D' or they have a GPA greater than 3.0:"
