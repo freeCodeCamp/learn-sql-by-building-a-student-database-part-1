dfa505a23d08d6ba27deb5be0eb975793c41dda2
Student9226 CS <sagarkulhare755@gmail.com>
2024-06-19 09:00:46 +0000

    First commit

---
+++ insert_data.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#Script to insert data from courses.csv and students.csv into students database
+PSQL="psql -X --username=freecodecamp --dbname=students --no-align --tuples-only -c"
+cat courses.csv | while IFS="," read MAJOR COURSE
+do
+  # get major_id
+  MAJOR_ID=$($PSQL "SELECT major_id FROM majors WHERE major='$MAJOR'")
+  # if not found
+
+  # insert major
+
+  # get new major_id
+
+  # get course_id
+
+  # if not found
+
+  # insert course
+
+  # get new course_id
+
+  # insert into majors_courses
+
+done
